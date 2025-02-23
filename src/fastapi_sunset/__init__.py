20ffcc63046ff830a49c562951d22a2a2dae4a76
Sebastian Correa <43179146+sebastian-correa@users.noreply.github.com>
2025-02-23 18:51:28 -0300

    Implement RespondError behavior

--- src/fastapi_sunset/__init__.py
+++ src/fastapi_sunset/__init__.py
@@ -1,11 +1,12 @@
 """A lightweight and RFC 8594 compliant FastAPI middleware to facilitate endpoint deprecation."""
 
-from fastapi_sunset.behaviors import BasePeriodBehavior, DoNothing, RedirectUsers
+from fastapi_sunset.behaviors import BasePeriodBehavior, DoNothing, RedirectUsers, RespondError
 from fastapi_sunset.configuration import SunsetConfiguration
 
 __all__ = [
     "BasePeriodBehavior",
     "DoNothing",
     "RedirectUsers",
+    "RespondError",
     "SunsetConfiguration",
 ]
